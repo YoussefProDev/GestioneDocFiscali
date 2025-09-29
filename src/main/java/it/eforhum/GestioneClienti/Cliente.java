a9ea7d28ba742ff56b08f0698c0945b3a1ea1d03
YoussefProDev <youssef23468210@gmail.com>
2025-09-29 13:02:55 +0200

    iniziata la gestione dei clienti

--- src/main/java/it/eforhum/GestioneClienti/Cliente.java
+++ src/main/java/it/eforhum/GestioneClienti/Cliente.java
@@ -1,5 +1,31 @@
 package it.eforhum.GestioneClienti;
 
 public class Cliente {
-    
+    private String id;
+    private String denominazione;
+    private String partitaIva;
+    private String indirizzo;
+
+    public Cliente(String id, String denominazione, String partitaIva, String indirizzo) {
+        this.id = id;
+        this.denominazione = denominazione;
+        this.partitaIva = partitaIva;
+        this.indirizzo = indirizzo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDenominazione() {
+        return denominazione;
+    }
+
+    public String getPartitaIva() {
+        return partitaIva;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
 }
--- src/main/java/it/eforhum/GestioneClienti/GestioneClienti.java
+++ src/main/java/it/eforhum/GestioneClienti/GestioneClienti.java
@@ -1,5 +1,131 @@
 package it.eforhum.GestioneClienti;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GestioneClienti {
-    
+    private ArrayList<Cliente> allClienti;
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public GestioneClienti() {
+        this.allClienti = new ArrayList<>();
+        readallClienti();
+    }
+
+    public void printallClienti() {
+        System.out.println("Lista Clienti:");
+        allClienti.forEach(v -> System.out.println(
+                v.getId() + ") " + v.getDenominazione() + " - " + v.getPartitaIva() + " - " + v.getIndirizzo()));
+    }
+
+    public String chooseCliente() {
+        System.out.println("Scegli il Cliente:");
+        System.out.println("0) Nuovo Cliente");
+        allClienti.forEach(v -> System.out.println((allClienti.indexOf(v) + 1) + ") " + v.getDenominazione()));
+        Integer selection = numberInput("Scegliere Cliente: ", 0, allClienti.size());
+        Cliente Cliente;
+        if (selection == 0) {
+            Cliente = this.createCliente();
+        } else {
+            Cliente = allClienti.get(selection - 1);
+        }
+        System.out.println("Cliente scelto: " + Cliente.getDenominazione());
+        return Cliente.getId();
+    }
+
+    private Cliente createCliente() {
+        try {
+            File myObj = new File("Clientes.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File creato: " + myObj.getName());
+            } else {
+                System.out.println("File già esistente.");
+            }
+
+            FileWriter myWriter = new FileWriter("Clienti.txt", true);
+            String ClienteId = String.valueOf(allClienti.size() + 1);
+            boolean partitaIvaExist = false;
+            String denominazione = stringInput("Inserire la Denominazione: ");
+            String partitaIva;
+            do {
+                if (partitaIvaExist)
+                    System.out.println("Denominazione già esistente.");
+                partitaIvaExist = false;
+                partitaIva = stringInput("Inserire la Partita IVA: ");
+                for (Cliente Cliente : this.allClienti) {
+                    if (Cliente.getPartitaIva().equals(partitaIva))
+                        partitaIvaExist = true;
+                }
+            } while (partitaIvaExist);
+
+            String indirizzo = stringInput("Inserire l'Indirizzo: ");
+            Cliente Cliente = new Cliente(ClienteId, denominazione, partitaIva, indirizzo);
+            this.allClienti.add(Cliente);
+            myWriter.write(
+                    Cliente.getDenominazione() + "\t" + Cliente.getPartitaIva() + "\t" + Cliente.getIndirizzo() + "\t");
+            myWriter.close();
+            return Cliente;
+
+        } catch (IOException e) {
+            System.out.println("Si è verificato un errore.");
+            return this.createCliente();
+        }
+    }
+
+    public ArrayList<Cliente> getallClienti() {
+        return allClienti;
+    }
+
+    protected void readallClienti() {
+        ArrayList<Cliente> Clienti = new ArrayList<>();
+        try {
+            File myObj = new File("Clienti.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String[] data = myReader.nextLine().split("\t");
+                Clienti.add(new Cliente(data[0], data[1], data[2], data[3]));
+            }
+            myReader.close();
+            this.allClienti = Clienti;
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato.");
+        }
+    }
+
+    // --- METODI DI INPUT ---
+    private int numberInput(String text, int min, int max) {
+        while (true) {
+            System.out.print(text);
+            try {
+                int selection = Integer.parseInt(SCANNER.nextLine());
+                if (selection >= min && selection <= max)
+                    return selection;
+                else
+                    System.out.println("Numero fuori dal range, riprova.");
+            } catch (NumberFormatException e) {
+                System.out.println("Valore non valido, inserisci un numero.");
+            }
+        }
+    }
+
+    private String stringInput(String text) {
+        System.out.print(text);
+        return SCANNER.nextLine().trim();
+    }
+
+    private String stringInput(String text, String[] options) {
+        while (true) {
+            System.out.print(text);
+            String input = SCANNER.nextLine().trim();
+            for (String option : options) {
+                if (input.equalsIgnoreCase(option))
+                    return input;
+            }
+            System.out.println("Valore non valido. Opzioni valide: " + String.join(", ", options));
+        }
+    }
 }
