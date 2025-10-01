60be78fe67e6ff0a346fb15cfd36cd0131407c4a
YoussefProDev <youssef23468210@gmail.com>
2025-10-01 10:24:42 +0200

    aggiornato DocFiscale

--- src/main/java/it/eforhum/GestioneFatture/DocFiscale.java
+++ src/main/java/it/eforhum/GestioneFatture/DocFiscale.java
@@ -18,7 +18,7 @@ public class DocFiscale implements Comparable<DocFiscale> {
         this.importo = Double.parseDouble(parts[2]);
         this.descrizione = parts[3];
         this.data = LocalDate.parse(parts[4]);
-        this.id =  this.numeroFattura + "_" + this.data.getYear();
+        this.id = this.numeroFattura + "_" + this.data.getYear();
     }
 
     // Costruttore completo
@@ -34,22 +34,22 @@ public class DocFiscale implements Comparable<DocFiscale> {
     // Metodo per serializzare l'oggetto
     public String prepare() {
         return id + DELIMITER +
-               idCliente + DELIMITER +
-               importo + DELIMITER +
-               descrizione + DELIMITER +
-               data;
+                idCliente + DELIMITER +
+                importo + DELIMITER +
+                descrizione + DELIMITER +
+                data;
     }
 
     @Override
     public String toString() {
         return "DocFiscale{" +
-               "id='" + id + '\'' +
-               ", numero fattura='" + numeroFattura + '\'' +
-               ", descrizione='" + descrizione + '\'' +
-               ", data=" + data +
-               ", importo=" + importo +
-               ", idCliente=" + idCliente +
-               '}';
+                "id='" + id + '\'' +
+                ", numero fattura='" + numeroFattura + '\'' +
+                ", descrizione='" + descrizione + '\'' +
+                ", data=" + data +
+                ", importo=" + importo +
+                ", idCliente=" + idCliente +
+                '}';
     }
 
     // Implementazione di compareTo (ordine per data)
@@ -91,11 +91,11 @@ public class DocFiscale implements Comparable<DocFiscale> {
         this.importo = importo;
     }
 
-    public int getIdCliente() {
+    public String getIdCliente() {
         return idCliente;
     }
 
-    public void setIdCliente(int idCliente) {
+    public void setIdCliente(String idCliente) {
         this.idCliente = idCliente;
     }
 }
